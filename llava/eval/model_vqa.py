523852e166d5c7407966725eb863aabe972de370
Madhu Korada <manoj.korada0000@gmail.com>
2024-02-28 23:38:38 +0000

    Refactor VQA model evaluation code

--- llava/eval/model_vqa.py
+++ llava/eval/model_vqa.py
@@ -32,17 +32,34 @@ def eval_model(args):
     model_path = os.path.expanduser(args.model_path)
     model_name = get_model_name_from_path(model_path)
     tokenizer, model, image_processor, context_len = load_pretrained_model(model_path, args.model_base, model_name)
-
+    #print(args.question_file)    
     questions = [json.loads(q) for q in open(os.path.expanduser(args.question_file), "r")]
+    #questions = []
+    #for q in open(os.path.expanduser(args.question_file), "r"):
+    #    #print(q[-8:])
+    #    #if q[-8:] == '581829}':
+    #        #print("breaking")
+    #        #continue
+    #    q = json.loads(q)
+    #    questions.append(q)
+        
+
     questions = get_chunk(questions, args.num_chunks, args.chunk_idx)
     answers_file = os.path.expanduser(args.answers_file)
     os.makedirs(os.path.dirname(answers_file), exist_ok=True)
     ans_file = open(answers_file, "w")
     for line in tqdm(questions):
-        idx = line["question_id"]
-        image_file = line["image"]
-        qs = line["text"]
-        cur_prompt = qs
+        #print(line)
+        idx = line['question_id']
+        image_file = line['image_id']
+        # 000000580255
+        # append zeros to make it 12 char string
+        image_file = str(image_file).zfill(12)
+        image_file = 'COCO_val2014_' + image_file + '.jpg'
+        qs = line['question']
+        #cur_prompt = qs + 'Answer the question in a single word. i.e. output a single word.'
+        # cur_prompt = qs + '\n answer the question using a single word or phrase. You can use the template "Answer: <answer>"' 
+        cur_prompt = qs + '\nAnswer the question using a single word or phrase.'
         if model.config.mm_use_im_start_end:
             qs = DEFAULT_IM_START_TOKEN + DEFAULT_IMAGE_TOKEN + DEFAULT_IM_END_TOKEN + '\n' + qs
         else:
@@ -54,10 +71,8 @@ def eval_model(args):
         prompt = conv.get_prompt()
 
         input_ids = tokenizer_image_token(prompt, tokenizer, IMAGE_TOKEN_INDEX, return_tensors='pt').unsqueeze(0).cuda()
-
         image = Image.open(os.path.join(args.image_folder, image_file)).convert('RGB')
         image_tensor = process_images([image], image_processor, model.config)[0]
-
         with torch.inference_mode():
             output_ids = model.generate(
                 input_ids,
@@ -88,8 +103,8 @@ if __name__ == "__main__":
     parser.add_argument("--model-path", type=str, default="facebook/opt-350m")
     parser.add_argument("--model-base", type=str, default=None)
     parser.add_argument("--image-folder", type=str, default="")
-    parser.add_argument("--question-file", type=str, default="tables/question.jsonl")
-    parser.add_argument("--answers-file", type=str, default="answer.jsonl")
+    parser.add_argument("--question-file", type=str, default="llava/eval/table/question.jsonl")
+    parser.add_argument("--answers-file", type=str, default="answer-latest.jsonl")
     parser.add_argument("--conv-mode", type=str, default="llava_v1")
     parser.add_argument("--num-chunks", type=int, default=1)
     parser.add_argument("--chunk-idx", type=int, default=0)
@@ -99,3 +114,4 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     eval_model(args)
+
